f5203bdc42335a5a0ca08c8a25ebd5641ca935dd
白荻 <jijin.djj@alibaba-inc.com>
2019-10-28 22:04:16 +0800

    init

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import request from 'umi-request';
+import styles from './index.css';
+
+request('/api/test')
+  .then(res => {
+    console.log(JSON.stringify(res));
+  })
+  .catch(err => {
+    console.error(err);
+  })
+  .finally(() => {
+    console.log('finally');
+  })
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>
+          <a href="https://umijs.org/guide/getting-started.html">
+            Getting Started
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
